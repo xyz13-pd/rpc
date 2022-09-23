7c89fa3eef172350e0f1fb3c6ae64ddb5b38c528
inisire <inisire@gmail.com>
2022-09-23 11:49:23 +0300

    Add http redirect result

--- src/Http/Redirect.php
+++ src/Http/Redirect.php
@@ -8,14 +8,23 @@ use inisire\RPC\Result\ResultInterface;
 
 class Redirect implements ResultInterface, HttpResultInterface
 {
+    public function __construct(
+        private readonly string $location,
+        private readonly bool $permanent = true
+    )
+    {
+    }
+
     public function getHttpHeaders(): array
     {
-        return [];
+        return [
+            'Location' => $this->location
+        ];
     }
 
     public function getHttpCode(): int
     {
-        return 301;
+        return $this->permanent ? 301 : 307;
     }
 
     public function getOutput(): mixed
